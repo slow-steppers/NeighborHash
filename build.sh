7f3dee01f55923b8adf87b91d168fb7a6e3c1709
Disheng WU <wudisheng@bilibili.com>
2024-02-28 19:02:09 +0800

    Add build.sh.

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -eu
+
+ABSOLUTE_ME="$(realpath "${0}")"
+PROJECT_DIR="$(dirname "${ABSOLUTE_ME}")"
+
+cd -P "${PROJECT_DIR}"
+
+NEED_DOWNLOAD_BAZEL="1"
+if [ "$(type -t "bazel")" == "file" ]; then
+  SYSTEM_BAZEL_VERSION="$(bazel --version | head -n "1" | sed -n 's/^.*\s\+\(\([0-9]\+\.\)\+[0-9]\+[a-z]\?\)\(\s\+.*\)\?\s*$/\1/p')"
+  if [ "$(echo -ne "${SYSTEM_BAZEL_VERSION}\n6.3.0\n" | sort -V | head -n "1")" == "6.4.0" ]; then
+    NEED_DOWNLOAD_BAZEL="0"
+  fi
+fi
+
+BAZEL_BINARY="bazel"
+if [ "${NEED_DOWNLOAD_BAZEL}" -ne "0" ]; then
+  mkdir -p "bin"
+  curl -s -f -L -m "60" "https://github.com/bazelbuild/bazel/releases/download/6.3.2/bazel-6.3.2-linux-x86_64" > "bin/bazel"
+  chmod +x "bin/bazel"
+  BAZEL_BINARY="bin/bazel"
+fi
+
+ARGUMENTS=( --config="neighbor_hugepage" --config="neighbor_simd" )
+
+if [ "$(type -t "clang")" == "file" ]; then
+  ARGUMENTS+=( --config="clang" )
+fi
+
+exec "${BAZEL_BINARY}" build "${ARGUMENTS[@]}" "//testing:all" "${@}"
